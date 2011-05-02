2354c60fd3b7553e014d2ca2398c69c191284db8
Eric E Monson <emonson@cs.duke.edu>
2011-05-01 21:51:23 -0400

    Refined web pages a bit and added attachments to decision show

--- shows/decision.js
+++ shows/decision.js
@@ -10,12 +10,21 @@ function(doc, req) {
     var markdown = require("vendor/couchapp/lib/markdown");
     var html = markdown.encode(doc.content);
     
+    var attch = doc._attachments;
+    var att_list = [];
+    for (var a in attch) {
+      var att_url = '../../../../' + doc._id + "/" + a;
+      var att_obj = {"att_url":att_url, "att_name":a};
+      att_list.push(att_obj);
+    }
+    
     var data = {
 	    "date": doc.date,
 	    "type": doc.type,
-	    "content": html
+	    "content": html,
+	    "attachments": att_list
     };
-    
+        
     return mustache.to_html(this.templates.decision, data);
 
 }
